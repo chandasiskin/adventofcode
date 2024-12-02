2b6a3d2508188012f076b8b89681c9d65711fa85
chanda <chanda.siskin@hotmail.com>
2024-12-02 22:52:27 +0100

    Merged solutions of part 1 and part 2

--- 2024/Day02.php
+++ 2024/Day02.php
@@ -1,33 +1,36 @@
 <?php
     namespace adventOfCode;
     
-    class Day02 {
+    class Day02
+    {
         private string $input;
         private float $startTime;
         private array $reports;
         
-        public function __construct(string $path) {
+        public function __construct(string $path)
+        {
             $this->startTime = microtime(true);
             
             $this->input = trim(file_get_contents($path));
         }
         
-        public function solve(): void {
+        public function solve(): void
+        {
             $this->buildReports();
             
-            $sol1 = $this->solve1();
-            $sol2 = $this->solve2();
+            $sol = $this->solveBoth();
             
             $elapsedTime = microtime(true) - $this->startTime;
             $elapsedTimeMicroSeconds = $elapsedTime * 1000;
             
             echo
-                "Part 1: {$sol1}" . PHP_EOL .
-                "Part 2: {$sol2}" . PHP_EOL .
+                "Part 1: {$sol[0]}" . PHP_EOL .
+                "Part 2: {$sol[1]}" . PHP_EOL .
                 "Time: {$elapsedTimeMicroSeconds}ms";
         }
         
-        private function buildReports(): void {
+        private function buildReports(): void
+        {
             $reports = preg_split("/[\r\n]+/", $this->input);
             
             $this->reports = array_fill(0, count($reports), []);
@@ -39,7 +42,8 @@
             }
         }
         
-        private function isSafe(array $levels): bool {
+        private function isSafe(array $levels): bool
+        {
             if ($levels[0] === $levels[1]) {
                 return false;
             }
@@ -64,24 +68,15 @@
             return true;
         }
         
-        private function solve1(): int {
-            $safeReports = 0;
+        private function solveBoth(): array
+        {
+            $safeReports_part_1 = 0;
+            $safeReports_part_2 = 0;
             
             foreach ($this->reports as $levels) {
                 if ($this->isSafe($levels)) {
-                    $safeReports++;
-                }
-            }
-            
-            return $safeReports;
-        }
-        
-        private function solve2(): int {
-            $safeReports = 0;
-            
-            foreach ($this->reports as $levels) {
-                if ($this->isSafe($levels)) {
-                    $safeReports++;
+                    $safeReports_part_1++;
+                    $safeReports_part_2++;
                 } else {
                     $length = count($levels);
                     
@@ -90,14 +85,14 @@
                         array_splice($tmp, $i, 1);
                         
                         if ($this->isSafe($tmp)) {
-                            $safeReports++;
+                            $safeReports_part_2++;
                             break;
                         }
                     }
                 }
             }
             
-            return $safeReports;
+            return [$safeReports_part_1, $safeReports_part_2];
         }
     }
     
